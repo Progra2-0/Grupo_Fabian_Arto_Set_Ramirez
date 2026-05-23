5f2bde16c34b0a848ef87f213de6304cbaa3b22c
Progra2-0 <set.ramirez.c@mail.pucv.cl>
2026-05-23 16:24:52 -0400

    Add files via upload

---
+++ Proyectos/Proyecto1-Python/analisis.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def comparar_rendimiento(datos): 
+
+    arr = np.array(datos)
+    nombres = np.unique(arr[:, 1])
+    resultados = {}
+
+    for nombre in nombres:       
+        
+        mask = arr[:, 1] == nombre
+        
+       
+        bateria_filtrada = arr[mask, 4]
+        bateria = bateria_filtrada.astype(float)
+        
+        basura_filtrada = arr[mask, 5]
+        basura = basura_filtrada.astype(float)
+        
+        
+        ultima_bateria = bateria[-1]
+        consumo = 100.0 - ultima_bateria
+        
+        basura_total = basura[-1]
+        
+        
+        if consumo > 0:
+            eficiencia = basura_total / consumo
+        else:
+            eficiencia = 0.0
+            
+        resultados[nombre] = {
+            'consumo_bateria': consumo,
+            'basura_total': basura_total,
+            'eficiencia': eficiencia
+        }
+
+    return resultados
